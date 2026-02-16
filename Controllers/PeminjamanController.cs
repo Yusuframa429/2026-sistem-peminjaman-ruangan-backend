aefad2c643f902e278ff587fb833026e8d103654
Yusuf R A <yusuframadhaniarianto@gmail.com>
2026-02-16 13:40:30 +0700

    modifikasi logika pencarian

--- Controllers/PeminjamanController.cs
+++ Controllers/PeminjamanController.cs
@@ -16,36 +16,22 @@ namespace _2026_sistem_peminjaman_ruangan_backend.Controllers
         {
             _context = context;
         }
-
-        // ==========================================
-        // 1. GET: Ambil data (BISA SEARCH & FILTER)
-        // ==========================================
-        // Perubahan ada di sini: Menambahkan parameter (string? q, string? status)
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Peminjaman>>> GetPeminjaman(string? q, string? status)
         {
-            // Siapkan query database
             var query = _context.Peminjaman.AsQueryable();
-
-            // LOGIKA PENCARIAN (Search)
-            // Jika ada 'q', cari di NamaPeminjam ATAU Ruangan
             if (!string.IsNullOrEmpty(q))
             {
                 query = query.Where(x => x.NamaPeminjam.Contains(q) || x.Ruangan.Contains(q));
             }
-
-            // LOGIKA FILTER (Penyaringan)
-            // Jika ada 'status', ambil yang statusnya cocok
             if (!string.IsNullOrEmpty(status))
             {
                 query = query.Where(x => x.Status == status);
             }
-
-            // Jalankan query dan kembalikan hasil
             return await query.ToListAsync();
         }
-
-        // 2. GET: Ambil detail 1 peminjaman
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<Peminjaman>> GetPeminjaman(int id)
         {
@@ -53,8 +39,7 @@ namespace _2026_sistem_peminjaman_ruangan_backend.Controllers
             if (peminjaman == null) return NotFound();
             return peminjaman;
         }
-
-        // 3. POST: Tambah Data Baru
+        
         [HttpPost]
         public async Task<ActionResult<Peminjaman>> PostPeminjaman(CreatePeminjamanDto request)
         {
@@ -72,8 +57,7 @@ namespace _2026_sistem_peminjaman_ruangan_backend.Controllers
 
             return CreatedAtAction(nameof(GetPeminjaman), new { id = peminjaman.Id }, peminjaman);
         }
-
-        // 4. PUT: Update Status
+        
         [HttpPut("{id}/status")]
         public async Task<IActionResult> UpdateStatus(int id, [FromBody] string statusBaru)
         {
@@ -85,8 +69,7 @@ namespace _2026_sistem_peminjaman_ruangan_backend.Controllers
 
             return NoContent();
         }
-
-        // 5. DELETE: Hapus Data
+        
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePeminjaman(int id)
         {
